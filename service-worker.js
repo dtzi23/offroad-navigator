2758178f22e83394d42529d86b910342aa50e025
dtzi23 <dennisdtzi@gmail.com>
2026-05-10 22:30:58 +0200

    Fix: vervang turf.js door inline Haversine, bump SW cache naar v2

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
-const CACHE = 'offroad-v1';
+const CACHE = 'offroad-v2';
 const ASSETS = [
     '/OFF-ROAD-NAVIGATOR.html',
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet-draw/1.0.4/leaflet.draw.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet-draw/1.0.4/leaflet.draw.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/turf.js/6.5.0/turf.min.js',
 ];
 
 self.addEventListener('install', e => {
